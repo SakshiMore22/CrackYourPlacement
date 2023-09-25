f046bfe1eae4370f962d2477c6f460036ea94f91
Sakshi More <108238805+SakshiMore22@users.noreply.github.com>
2023-09-25 16:37:47 +0530

    Time: 209 ms (6.9%), Space: 18.6 MB (5.07%) - LeetHub

--- 0815-champagne-tower/0815-champagne-tower.py
+++ 0815-champagne-tower/0815-champagne-tower.py
@@ -1,29 +1,26 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-#stupid test
-        if poured == 0: return 0
-#defenition of the size of rectangles
-        n = query_row-query_glass+1
-        m = query_glass+1
-        n, m = max(n,m), min(n,m)
-#defifnition of value in the first column of rectangle
-        dp = [0]*n
-        dp[0] = poured
-        for i in range(n-1): dp[i+1] = max((dp[i]-1)/2,0)
-#evolution of column of rectangle
-        for j in range(1,m):
-            for i in range(n):
-                if i == 0: dp[i] = max((dp[i]-1)/2,0)
-                else:  dp[i] = max((dp[i]-1)/2,0) + max((dp[i-1]-1)/2,0)
-        return min(dp[-1],1)
-
-            
-
-
-
+        def calc(cap,r,c):
+            if r==query_row and c==query_glass:
+                return cap
+            if r>query_row or c>query_glass or query_glass>query_row:
+                return 0.00
+            # if cap<0:
+            #     return 0.0
+            if dp[r][c]!=-1:
+                return dp[r][c]
+            lef=(calc(cap,r+1,c)-1)/2.0
+            rig=(calc(cap,r+1,c+1)-1)/2.0
+            if lef<0:
+                lef=0
+            if rig<0:
+                rig=0
+            dp[r][c]=lef+rig
+            return dp[r][c]
+        dp=[[-1 for i in range(101)] for j in range(101)]
+        return min(1.0,calc(poured,0,0))
 
 
-            
-            
+        
         
         
