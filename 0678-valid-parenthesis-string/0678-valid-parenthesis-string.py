9dd76b36269a03b1dcad73e1e0d3acc0bf371722
Sakshi More <108238805+SakshiMore22@users.noreply.github.com>
2023-09-30 00:46:39 +0530

    Time: 65 ms (5.18%), Space: 16.3 MB (34.53%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,24 +1,22 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        def calc(ind,op):
-            if ind==len(s):
-                return op==0
-            if dp[ind][op]!=-1:
-                return dp[ind][op]
-            ans=False
-            if s[ind]=='(':
-                ans=ans or calc(ind+1,op+1)
-            elif s[ind]==')':
-                if op:
-                    ans=ans or calc(ind+1,op-1)
-            else:
-                ans=ans or calc(ind+1,op+1)
-                if op:
-                    ans=ans or calc(ind+1,op-1)
-                ans=ans or calc(ind+1,op)
-            dp[ind][op]=ans
-            return dp[ind][op]
-        dp=[[-1 for i in range(len(s))]for j in range(len(s))]
-        return calc(0,0)
+        dp=[[False for i in range(len(s)+1)]for j in range(len(s)+1)]
+        dp[len(s)][0]=True
+        for ind in range(len(s)-1,-1,-1):
+            for op in range(len(s)):
+                ans=False
+                if s[ind]=='(':
+                    ans=ans or dp[ind+1][op+1]
+                elif s[ind]==')':
+                    if op:
+                        ans=ans or dp[ind+1][op-1]
+                else:
+                    ans=ans or dp[ind+1][op+1]
+                    if op:
+                        ans=ans or dp[ind+1][op-1]
+                    ans=ans or dp[ind+1][op]
+                dp[ind][op]=ans
+        return dp[0][0]
 
+        
         
