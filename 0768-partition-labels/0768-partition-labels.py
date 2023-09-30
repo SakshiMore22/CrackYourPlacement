7bd98068286436c3ec82dd4d33384a07637039cd
Sakshi More <108238805+SakshiMore22@users.noreply.github.com>
2023-09-30 15:15:13 +0530

    Time: 43 ms (71.47%), Space: 16 MB (98.52%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -1,38 +1,16 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         dic={}
-        for i in s:
-            if i in dic:
-                dic[i]+=1
-            else:
-                dic[i]=1
-        di={}
-        i,j=0,0
+        for i in range(len(s)):
+            dic[s[i]]=i
+        prev=-1
+        maxi=0
         ans=[]
-        while j<len(s):
-            if s[j] in di:
-                di[s[j]]+=1
-            else:
-                if len(di)>0:
-                    c=0
-                    for k in di:
-                        if dic[k]==di[k]:
-                            c+=1
-                    if c==len(di):
-                        ans.append(j-i)
-                        di={s[j]:1}
-                        i=j
-                    else:
-                        di[s[j]]=1
-                else:
-                    di[s[j]]=1
-                
-            j+=1
-            # print(di)
-        if len(di)>0:
-            ans.append(j-i)
+        for i in range(len(s)):
+            maxi=max(maxi,dic[s[i]])
+            if maxi==i:
+                ans.append(maxi-prev)
+                prev=maxi
         return ans
-
-
 
         
