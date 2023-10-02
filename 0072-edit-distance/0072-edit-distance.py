2f312259a5e6fb982c95cbe4fb098df5aa8e415f
Sakshi More <108238805+SakshiMore22@users.noreply.github.com>
2023-10-03 00:36:59 +0530

    Time: 133 ms (35.99%), Space: 20 MB (37.92%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,18 +1,15 @@
 from collections import Counter
 class Solution:
     def minDistance(self, text1: str, text2: str) -> int:
-        def calc(ind1,ind2):
-            if ind2<0:
-                return ind1+1
-            if ind1<0:
-                return ind2+1
-            if dp[ind1][ind2]!=-1:
-                return dp[ind1][ind2]
-            if text1[ind1]==text2[ind2]:
-                dp[ind1][ind2]=0+calc(ind1-1,ind2-1)
-                return dp[ind1][ind2]
-            else:
-                dp[ind1][ind2]=min(1+calc(ind1-1,ind2-1),1+calc(ind1-1,ind2),1+calc(ind1,ind2-1))
-                return dp[ind1][ind2]
-        dp=[[-1 for i in range(len(text2)+1)] for j in range(len(text1)+1)]
-        return calc(len(text1)-1,len(text2)-1)
+        dp=[[0 for i in range(len(text2)+1)] for j in range(len(text1)+1)]
+        for i in range(len(text1)+1):
+            dp[i][0]=i
+        for i in range(len(text2)+1):
+            dp[0][i]=i
+        for ind1 in range(1,len(text1)+1):
+            for ind2 in range(1,len(text2)+1):
+                if text1[ind1-1]==text2[ind2-1]:
+                    dp[ind1][ind2]=0+dp[ind1-1][ind2-1]
+                else:
+                    dp[ind1][ind2]=min(1+dp[ind1-1][ind2-1],1+dp[ind1-1][ind2],1+dp[ind1][ind2-1])
+        return dp[len(text1)][len(text2)]
