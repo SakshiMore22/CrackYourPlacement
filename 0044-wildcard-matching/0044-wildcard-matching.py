ce15f0d8eb63c466b456428c975161d46da1c4ed
Sakshi More <108238805+SakshiMore22@users.noreply.github.com>
2023-10-03 00:46:01 +0530

    Time: 672 ms (20.78%), Space: 25 MB (56.5%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,24 +1,19 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        def calc(ind1,ind2):
-            if ind1<0 and ind2<0:
-                return True
-            if ind2<0 and ind1>=0:
-                return False
-            if ind1<0 and ind2>=0:
-                return all(c == '*' for c in p[:ind2+1])
-            if dp[ind1][ind2]!=-1:
-                return dp[ind1][ind2]
-            if s[ind1]==p[ind2] or p[ind2]=='?': 
-                dp[ind1][ind2]=calc(ind1-1,ind2-1)
-                return dp[ind1][ind2]
-            elif p[ind2]=='*':
-                dp[ind1][ind2]=(calc(ind1,ind2-1) or calc(ind1-1,ind2))
-                return dp[ind1][ind2]
-            else:
-                dp[ind1][ind2]=False
-                return dp[ind1][ind2]
-        dp=[[-1 for i in range(len(p))] for j in range(len(s))]
-        return calc(len(s)-1,len(p)-1)
+        dp=[[False for i in range(len(p)+1)] for j in range(len(s)+1)]
+        dp[0][0]=True
+        for i in range(1,len(s)+1):
+            dp[i][0]=False
+        for i in range(1,len(p)+1):
+            dp[0][i]=all(c == '*' for c in p[:i])
+        for ind1 in range(1,len(s)+1):
+            for ind2 in range(1,len(p)+1):
+                if s[ind1-1]==p[ind2-1] or p[ind2-1]=='?': 
+                    dp[ind1][ind2]=dp[ind1-1][ind2-1]
+                elif p[ind2-1]=='*':
+                    dp[ind1][ind2]=(dp[ind1][ind2-1] or dp[ind1-1][ind2])
+                else:
+                    dp[ind1][ind2]=False
+        return dp[len(s)][len(p)]
 
         
