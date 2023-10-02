455a58b54132f5a2125b39cd14e30de4163c029d
Sakshi More <108238805+SakshiMore22@users.noreply.github.com>
2023-10-02 17:43:01 +0530

    Time: 431 ms (54.1%), Space: 73 MB (51.82%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,17 +1,12 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        def calc(ind1,ind2):
-            if ind1<0:
-                return int(ind2<0)
-            if ind2<0:
-                return 1
-            if dp[ind1][ind2]!=-1:
-                return dp[ind1][ind2]
-            if s[ind1]==t[ind2]:
-                dp[ind1][ind2]=(calc(ind1-1,ind2-1)+calc(ind1-1,ind2))
-                return dp[ind1][ind2]
-            else:
-                dp[ind1][ind2]=calc(ind1-1,ind2)
-                return dp[ind1][ind2]
-        dp=[[-1 for i in range(len(t))] for j in range(len(s))]
-        return calc(len(s)-1,len(t)-1)
+        dp=[[0 for i in range(len(t)+1)] for j in range(len(s)+1)]
+        for i in range(len(s)+1):
+            dp[i][0]=1
+        for ind1 in range(1,len(s)+1):
+            for ind2 in range(1,len(t)+1):
+                if s[ind1-1]==t[ind2-1]:
+                    dp[ind1][ind2]=(dp[ind1-1][ind2-1]+dp[ind1-1][ind2])
+                else:
+                    dp[ind1][ind2]=dp[ind1-1][ind2]
+        return dp[len(s)][len(t)]
